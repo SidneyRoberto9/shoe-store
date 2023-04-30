d46e9c39649e5ac1cd9478a51b38af5def887e9f
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-04-30 02:27:57 -0300

    🔧 ci: Add tailwind font configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    fontFamily: {
+      oswald: 'Oswald, sans-serif',
+      urbanist: 'Urbanist, sans-serif',
+    },
     extend: {},
   },
   plugins: [],
