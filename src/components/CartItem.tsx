77559e55fc49ad8643aa34dcb5e68c5095957673
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-05-02 22:27:20 -0300

    📦 refactor: Change cartItem for consume api

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,65 +1,91 @@
+import Image from 'next/image';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 
-export function CartItem() {
+import { removeFromCart, updateCart } from '@/store/cartSlice';
+import { useAppDispatch } from '@/store/hooks';
+import { formatPrice } from '@/utils/helper';
+
+interface CartItemProps {
+  data: any;
+}
+
+export function CartItem({ data }: CartItemProps) {
+  const productAttributes = data.attributes;
+  const thumbnailUrl = productAttributes.thumbnail.data.attributes.url;
+  const productSize = productAttributes.size.data;
+
+  const dispatch = useAppDispatch();
+
+  function updateCartItem(item: any, key: string) {
+    let payload = {
+      key,
+      val: key === 'quantity' ? parseInt(item.target.value) : item.target.value,
+      id: data.id,
+    };
+
+    dispatch(updateCart(payload));
+  }
+
   return (
     <div className="flex py-5 gap-3 md:gap-5 border-b">
       <div className="shrink-0 aspect-square w-[50px] md:w-[120px]">
-        <img src="/assets/product-1.webp" />
+        <Image width={120} height={120} src={thumbnailUrl} alt={productAttributes.name} />
       </div>
 
       <div className="w-full flex flex-col">
         <div className="flex flex-col md:flex-row justify-between">
-          <div className="text-lg md:text-2xl font-semibold text-black/[0.8]">Jordan Retro 6 G</div>
+          <div className="text-lg md:text-2xl font-semibold text-black/[0.8]">
+            {productAttributes.name}
+          </div>
           <div className="text-sm md:text-md font-medium text-black/[0.5] block md:hidden">
-            Men&apos;s Golf Shoes
+            {productAttributes.subtitle}
           </div>
 
           <div className="text-sm md:text-md font-bold text-black/[0.5] mt-2">
-            MRP : $ 19 695.00
+            {formatPrice(productAttributes.price)}
           </div>
         </div>
 
         <div className="text--md font-medium text-black/[0.5] hidden md:block">
-          Men&apos;s Golf Shoes
+          {productAttributes.subtitle}
         </div>
 
         <div className="flex items-center justify-between mt-4">
           <div className="flex items-center gap-2 md:gap-10 text-black/[0.5] text-sm md:text-md">
             <div className="flex items-center gap-1">
               <div className="font-semibold">Size:</div>
-              <select className="hover:text-black">
-                <option value="1">UK 6</option>
-                <option value="2">UK 6.5</option>
-                <option value="3">UK 7</option>
-                <option value="4">UK 7.5</option>
-                <option value="5">UK 8</option>
-                <option value="6">UK 8.5</option>
-                <option value="7">UK 9</option>
-                <option value="8">UK 9.5</option>
-                <option value="9">UK 10</option>
-                <option value="10">UK 10.5</option>
-                <option value="11">UK 11</option>
+              <select
+                className="hover:text-black"
+                onChange={(e) => updateCartItem(e, 'selectSize')}
+              >
+                {productSize.map((item: any, index: number) => (
+                  <option
+                    key={index}
+                    value={item.size}
+                    disabled={!item.enabled ? true : false}
+                    selected={item.size === data.selectSize}
+                  >
+                    {item.size}
+                  </option>
+                ))}
               </select>
             </div>
             <div className="flex items-center gap-1">
               <div className="font-semibold">Quantity:</div>
-              <select className="hover:text-black">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
+              <select className="hover:text-black" onChange={(e) => updateCartItem(e, 'quantity')}>
+                {Array.from({ length: 10 }, (_, i) => i + 1).map((item, index) => (
+                  <option key={index} value={item} selected={data.quantity === item}>
+                    {item}
+                  </option>
+                ))}
               </select>
             </div>
           </div>
 
-          <RiDeleteBin6Line className="cursor-pointer text-black/[0.5] hover:text-black text-[16px] md:text-[20px]" />
+          <RiDeleteBin6Line
+            className="cursor-pointer text-black/[0.5] hover:text-black text-[16px] md:text-[20px]"
+            onClick={() => dispatch(removeFromCart({ id: data.id }))}
+          />
         </div>
       </div>
     </div>
