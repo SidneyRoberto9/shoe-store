28f48ee00d805b51c063cf0a8f7f298654859bb5
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-05-02 22:29:36 -0300

    📦 refactor: add Loading for pages

--- src/data/MenuRoutes.ts
+++ src/data/MenuRoutes.ts
@@ -6,10 +6,3 @@ export const routesMenu: RouteMenu[] = [
   { id: 3, name: 'Categories', subMenu: true },
   { id: 4, name: 'Contact', url: '/contact' },
 ];
-
-export const subMenuData: RouteSubMenu[] = [
-  { id: 1, name: 'Jordan', doc_count: 11 },
-  { id: 2, name: 'Sneakers', doc_count: 8 },
-  { id: 3, name: 'Running shoes', doc_count: 64 },
-  { id: 4, name: 'Football shoes', doc_count: 107 },
-];
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,48 @@
 import '@/styles/globals.css';
 
 import Head from 'next/head';
+import { Provider } from 'react-redux';
 
 import { Footer } from '@/components/Footer/Index';
 import { Header } from '@/components/Header/Index';
+import { store } from '@/store/store';
 
 import type { AppProps } from 'next/app';
+import { NextRouter, useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { Loading } from '../components/Loading';
+
 export default function App({ Component, pageProps }: AppProps) {
+  const router: NextRouter = useRouter();
+
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleStart = (url: string) => url !== router.asPath && setLoading(true);
+    const handleComplete = (url: string) => url === router.asPath && setLoading(false);
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
         <title>Online Shoe Store</title>
       </Head>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
+      <Loading load={loading} />
+      <Provider store={store}>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </Provider>
     </>
   );
 }
