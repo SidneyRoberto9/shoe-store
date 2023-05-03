5889bf9ede6d01eb0e504b03d47be74ffdd58068
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-05-03 06:02:28 -0300

    ✨ feat: Populate about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,3 +1,26 @@
 export default function About() {
-  return <div>about</div>;
+  return (
+    <section className="py-20">
+      <div className="container mx-auto px-4">
+        <h1 className="font-bold text-4xl mb-8">About the Store</h1>
+        <p className="text-gray-800 leading-loose mb-8">
+          At Shoe Store, we are passionate about providing the latest and greatest AirJordan
+          sneakers and apparel to our customers. Our online store offers a vast selection of
+          AirJordan products, from iconic sneaker styles to trendy clothing options. We are
+          committed to delivering exceptional customer service and creating an enjoyable shopping
+          experience for every customer.
+        </p>
+        <p className="text-gray-800 leading-loose mb-8">
+          Our team of experienced professionals is dedicated to bringing you the highest quality
+          products and staying up-to-date with the latest AirJordan releases. We believe that
+          everyone should have access to the sneakers and apparel they love, and we strive to make
+          that a reality for our customers.
+        </p>
+        <p className="text-gray-800 leading-loose mb-8">
+          Thank you for choosing Shoe Store as your go-to destination for AirJordan products. We
+          look forward to serving you and providing you with the best possible shopping experience.
+        </p>
+      </div>
+    </section>
+  );
 }
